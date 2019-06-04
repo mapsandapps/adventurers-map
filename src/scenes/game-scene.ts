2972ac25f41102324231300afcbd8774d66ff52c
Shawn Taylor <shawn@mapsandapps.net>
2019-06-03 20:56:10 -0400

    Add player

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -1,7 +1,11 @@
+import { Player } from '../objects/player'
+
 export class GameScene extends Phaser.Scene {
   private layer: Phaser.Tilemaps.StaticTilemapLayer
   private map: Phaser.Tilemaps.Tilemap
   private tileset: Phaser.Tilemaps.Tileset
+
+  private player: Player
 
   constructor() {
     super({
@@ -17,5 +21,20 @@ export class GameScene extends Phaser.Scene {
     this.tileset = this.map.addTilesetImage('RPGpack_sheet')
     this.layer = this.map.createStaticLayer('Map', this.tileset, 0, 0)
     this.layer.setCollisionByProperty({ collide: true })
+
+    this.player = new Player({
+      scene: this,
+      x: 128,
+      y: 128,
+      key: 'player'
+    })
+
+    this.physics.add.collider(this.player, this.layer)
+
+    this.cameras.main.startFollow(this.player)
+  }
+
+  update(): void {
+    this.player.update()
   }
 }
